055ae2c639003a9e553bdef8dc5b6c7b9adf20b6
Pedro Bianco <pedroalasticobianco@gmail.com>
2023-10-02 19:00:21 -0300

    Start project
    Start project

---
+++ src/main/java/com/pedro/sugestaofilmes/sugestaofilmes/SugestaofilmesApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.sugestaofilmes.sugestaofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugestaofilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugestaofilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/sugestaofilmes/sugestaofilmes/SugestaofilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.sugestaofilmes.sugestaofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugestaofilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
